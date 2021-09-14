a363bafc569859d010aa74a2c65e0ec3bfe52c08
Ricardo Tapia <rtapia@121corp.com>
2021-09-14 15:41:05 -0500

    se integra un tema de tipoo blog esto con la intension de que este sitio quede como un blog personal

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth'])->name('home');
 
 require __DIR__.'/auth.php';
 
